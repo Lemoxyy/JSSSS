168b6e3a4ff9f8252f73d70cab0aad59af1661d3
Lemoxyy <92450755+Lemoxyy@users.noreply.github.com>
2024-05-15 12:30:15 +0000

    45

--- 01_basics/02_DataType.js
+++ 01_basics/02_DataType.js
@@ -3,6 +3,27 @@
 
 // alert(3+3) // we are using nodejs, not browser
 
-console.log(3+3); 
+console.log(3+3);  // code readability should be hight
 
 console.log("LEMOXYY");
+
+let name = 'LEMOXYY'
+let age = 18
+let isLoggedIn = true
+let state;
+
+// number => 2 t0 power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined =>
+// symbol => unique
+
+// object 
+console.log(typeof undefined); //undefined
+console.log(typeof null); //object
+
+
+
+
