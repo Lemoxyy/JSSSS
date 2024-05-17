c50bef1ce3d13621cbd7166d9cde46e475e1702f
Lemoxyy <92450755+Lemoxyy@users.noreply.github.com>
2024-05-17 15:50:49 +0000

    Operations

--- 01_basics/03_coversionOperation.js
+++ 01_basics/03_coversionOperation.js
@@ -55,8 +55,23 @@ let str3 = str1  + str2
 console.log(str3);
 
 
-console.log('1' +2);
-console.log(1+'2');
-console.log('1' + 2 + 2);
-console.log(1 + 2 + '2');
-console.log("1" +  "2");
+// console.log('1' +2);
+// console.log(1+'2');
+// console.log('1' + 2 + 2);
+// console.log(1 + 2 + '2');
+// console.log("1" +  "2");
+
+
+
+// console.log(3 +(4 *(5%3)))
+
+console.log(true)
+
+let num1,num2,num3
+
+num1 = num2 = num3 = 2+2
+
+
+let gameCounter = 100
+gameCounter++;
+console.log(gameCounter);
