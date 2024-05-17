de12bba2b909c89f295510374321487bd1d42e64
Lemoxyy <92450755+Lemoxyy@users.noreply.github.com>
2024-05-17 13:44:57 +0000

    Confusion's

---
+++ 01_basics/03_coversionOperation.js
@@ -0,0 +1,62 @@
+let score = true
+
+console.log(typeof score);
+console.log(typeof(score));
+
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+
+// "33" => 33
+// "33abc" => Nan
+// true => 1; false => 0
+
+let isLoggedIn = "LEMOXYY"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+
+// 1 => true
+// 0 => false 
+// "" => false
+// "lemoxyy" => true
+
+
+let someNUmber = 33
+
+let stringNumber = String(someNUmber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
+
+
+// ******************************* Operations ***************************************
+
+
+
+let value = 3
+let negValue = -value
+// console.log(negValue);
+
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2/2);
+// console.log(2*2);
+// console.log(2**2);
+// console.log(2%2);
+
+let str1 = 'hello'
+let str2 = ' Lemoxyy'
+let str3 = str1  + str2
+console.log(str3);
+
+
+console.log('1' +2);
+console.log(1+'2');
+console.log('1' + 2 + 2);
+console.log(1 + 2 + '2');
+console.log("1" +  "2");
