1605cfba80892233e3cfb0e114a03fcea609afd3
Lemoxyy <92450755+Lemoxyy@users.noreply.github.com>
2024-05-24 13:25:00 +0000

    datatypes summary

--- 01_basics/datatypes-summary.js
+++ 01_basics/datatypes-summary.js
@@ -1,3 +1,41 @@
 // Primitive
 
 // 7 types : String,Number,Boolean,null,undefined,object,symbol,bigInt
+
+const score = 100
+const scoreValue = 100.3
+
+const isLoggedIn = false
+const outsideTemp = null
+let userEmail;
+
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+console.log(id === anotherId);
+
+const bigNumber = 12315645645646546545645645645645643131n
+
+
+
+
+
+// Referemce (Non primitive)
+
+
+// Array,Object, Functions
+
+
+const heros = ['shaktiman','naagraj','iron man']
+let myObj = {
+    name: 'Lemoxyy',
+    age: 20,
+}
+
+let name = function(){
+    console.log("Hello World");
+}
+
+
+console.log(typeof(outsideTemp));
